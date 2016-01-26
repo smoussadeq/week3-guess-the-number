b87bef11e8bef2f4c097ba6e6b63cea41d7a9dab
Said Moussadeq <saidmoussadeq@saids-MacBook-Pro.local>
2016-01-26 09:52:16 -0500

    guess numbe6

--- guessTheNumberUpgrade.py
+++ guessTheNumberUpgrade.py
@@ -1,2 +1,5 @@
 # Said Moussadeqimport randomdef generateNumber(1,20)# Ask the player to guess 6 times. for guessesTaken in range(1, times+1):        print('Take your guess #’ + str(guessesTaken+’:’)
-        guess = int(input())        if userGuess < userSecretNumber:		print(‘Your guess is too low.’)		return False        elif userGuess > userSecretNumber:		print(‘ Your guess is too high.’)		return False        else:                return True        if userGuess = userSecretNumber        print('Good job! You guessed my number!')    else:        print('Nope. The number I was thinking of was ' + str(userSecretNumber))
+        guess = int(input())        if userGuess < userSecretNumber:		print(‘Your guess is too low.’)		return False        elif userGuess > userSecretNumber:		print(‘ Your guess is too high.’)		return False        else:                return True
+
+if( showAnswer == True):
+  print(‘—shhh, the real number is’+str(theNumber)+’.’)        if userGuess = userSecretNumber        print('Good job! You guessed my number!')    else:        print('Nope. The number I was thinking of was ' + str(userSecretNumber))
