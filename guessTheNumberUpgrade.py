6ae3d848055e677a92da5a845b10cc237f253a1e
Said Moussadeq <saidmoussadeq@saids-MacBook-Pro.local>
2016-01-28 23:01:59 -0500

    Revert "guess numb ups"
    This reverts commit 4113c1178a37e02258f5fcfb68105992f3b94384.

--- guessTheNumberUpgrade.py
+++ guessTheNumberUpgrade.py
@@ -1,5 +1,4 @@
-# Said Moussadeqimport random
-secretNumber = random.randint(1, 20)print(‘I am thinking of a number between 1 and 20.’)# Ask the player to guess 6 times. for guessesTaken in range(1, 7):        print('Take your guess #’ + str(guessesTaken+’:’)
+# Said Moussadeqimport randomdef generateNumber(1,20)# Ask the player to guess 6 times. for guessesTaken in range(1, 7):        print('Take your guess #’ + str(guessesTaken+’:’)
         guess = int(input())        if userGuess < userSecretNumber:		print(‘Your guess is too low.’)		return False        elif userGuess > userSecretNumber:		print(‘ Your guess is too high.’)		return False        else:                return True
 
 if( showAnswer == True):
